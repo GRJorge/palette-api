8fdb4af7df9d812d9c2259193f70f1ef8281198e
GRJorge <deb.devel@outlook.es>
2024-09-05 00:46:16 -0600

    User Routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
 
+import userRouter from './routes/user.routes';
+
 const app = express();
+
+app.use('/user', userRouter);
 
 export default app;
