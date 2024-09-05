210f0d0ab6d958be6b24f7a51def0565694cf13b
GRJorge <deb.devel@outlook.es>
2024-09-05 01:39:39 -0600

    Favorite Palettes not mandatory

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -3,5 +3,5 @@ import { Types } from 'mongoose';
 export default interface user {
     username: string;
     password: string;
-    favoritePalettes: [Types.ObjectId];
+    favoritePalettes?: [Types.ObjectId];
 }
