099d0c396419c1a18337db0e8f521d8402826236
GRJorge <deb.devel@outlook.es>
2024-09-05 01:36:03 -0600

    CORS Installation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 import userRouter from './routes/user.routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use('/user', userRouter);
 
