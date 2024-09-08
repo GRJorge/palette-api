30af22475b53ad3c296f6a51524bbd4a88adcea9
GRJorge <deb.devel@outlook.es>
2024-09-08 00:32:33 -0600

    Functions to compare and hash they passwords

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,14 @@
+import bcrypt from 'bcrypt';
 import userModel from '../models/user.model';
 import user from '../interfaces/user.interface';
 
 class UserService {
-    createUser(user: user) {
+    async createUser(user: user) {
+        user.password = await this.hashPassword(user.password);
+
         return new userModel(user).save();
     }
-    editUser(id: string, username?: string, password?: string) {
+    async editUser(id: string, username?: string, password?: string) {
         interface EditedData {
             username?: string;
             password?: string;
@@ -17,10 +20,22 @@ class UserService {
             editedData.username = username;
         }
         if (password) {
-            editedData.password = password;
+            editedData.password = await this.hashPassword(password);
         }
 
         return userModel.findByIdAndUpdate(id, editedData);
+    }
+    hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, 10);
+    }
+    async comparePassword(id: string, password: string): Promise<boolean> {
+        const query = await userModel.findById(id).select('password');
+
+        if (query) {
+            return bcrypt.compare(password, query.password);
+        } else {
+            return false;
+        }
     }
 }
 
