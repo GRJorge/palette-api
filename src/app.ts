9dc23d0fb6ba79e0eb3cb9bda01a0a5914061b08
GRJorge <deb.devel@outlook.es>
2024-09-18 00:01:12 -0600

    Morgan configuration

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import morgan from 'morgan';
 
 import userRouter from './routes/user.routes';
 import tagRouter from './routes/tag.routes';
+import paletteRouter from './routes/palette.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 app.use('/user', userRouter);
 app.use('/tag', tagRouter);
+app.use('/palette', paletteRouter);
 
 export default app;
