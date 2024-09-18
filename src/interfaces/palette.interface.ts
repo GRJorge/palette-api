4a8568e41ce1e710d18dc2f4df82f3383ea883fa
GRJorge <deb.devel@outlook.es>
2024-09-17 23:47:28 -0600

    Changed typed of palette and colors

--- src/interfaces/palette.interface.ts
+++ src/interfaces/palette.interface.ts
@@ -2,13 +2,13 @@ import { Types } from 'mongoose';
 
 export default interface palette {
     user: Types.ObjectId;
-    dark: string;
-    light: string;
-    primary: string;
-    secondary: string;
-    alert: string;
-    warning: string;
-    success: string;
+    dark: color;
+    light: color;
+    primary: color;
+    secondary: color;
+    alert: color;
+    warning: color;
+    success: color;
     names: [string];
     tags: [Types.ObjectId];
 }
@@ -21,4 +21,19 @@ export interface colors {
     alert: string;
     warning: string;
     success: string;
+    names: [string];
+}
+
+export interface color {
+    hex: string;
+    rgb: {
+        r: number;
+        g: number;
+        b: number;
+    };
+    hsl: {
+        h: number;
+        s: number;
+        l: number;
+    };
 }
--- src/models/palette.model.ts
+++ src/models/palette.model.ts
@@ -1,16 +1,30 @@
 import { Schema, model, Types } from 'mongoose';
-import palette from '../interfaces/palette.interface';
+import palette, { color } from '../interfaces/palette.interface';
+
+const colorSchema = new Schema<color>({
+    hex: { type: String, required: true },
+    rgb: {
+        r: { type: Number, required: true },
+        g: { type: Number, required: true },
+        b: { type: Number, required: true },
+    },
+    hsl: {
+        h: { type: Number, required: true },
+        s: { type: Number, required: true },
+        l: { type: Number, required: true },
+    },
+});
 
 const schema = new Schema<palette>(
     {
         user: { type: Schema.Types.ObjectId, ref: 'User' },
-        dark: { type: String, required: true },
-        light: { type: String, required: true },
-        primary: { type: String, required: true },
-        secondary: { type: String, required: true },
-        alert: { type: String, required: true },
-        warning: { type: String, required: true },
-        success: { type: String, required: true },
+        dark: { type: colorSchema, required: true },
+        light: { type: colorSchema, required: true },
+        primary: { type: colorSchema, required: true },
+        secondary: { type: colorSchema, required: true },
+        alert: { type: colorSchema, required: true },
+        warning: { type: colorSchema, required: true },
+        success: { type: colorSchema, required: true },
         names: { type: [String], required: true },
         tags: [{ type: Types.ObjectId, ref: 'Tag' }],
     },
